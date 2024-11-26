8bec3cab9808f60c396111d0916f78dc5255affb
WildHonestFur <137202612+WildHonestFur@users.noreply.github.com>
2024-11-26 06:23:10 -0800

    Update Server.py
    Fixed Errors, Added Features

--- Server.py
+++ Server.py
@@ -14,17 +14,16 @@ info = []
 dead = []
 num = {1: 'first', 2: 'second', 3: 'third', 4: 'fourth', 5: 'fifth', 6: 'sixth', 7: 'seventh', 8: 'eighth', 9: 'ninth',
        10: 'tenth'}
+started = False
 
 
 def start():
     while True:
         if input('Command: ') == 'START':
             for pl in players:
-                to_send = 'START'.encode('utf-8')
-                to_send = zlib.compress(to_send)
-                sock.sendto(to_send, players[pl])
-            global left
-            left = len(players)
+                ts = 'START'.encode('utf-8')
+                ts = zlib.compress(ts)
+                sock.sendto(ts, players[pl])
 
 
 s = Thread(target=start)
@@ -33,11 +32,16 @@ s.start()
 while True:
     data, address = sock2.recvfrom(1024)
     data = data.decode('utf-8')
-    # To fix
-    """if len(info) == 1:
-        sock.sendto(bytes(f'WIN', 'utf-8'), players[int(info[0][6])])
+    if len(info) <= 1 and started:
+        try:
+            to_send = 'WIN'.encode('utf-8')
+            to_send = zlib.compress(to_send)
+            sock.sendto(to_send, players[int(info[0][6])])
+        except IndexError:
+            pass
         info = []
-        dead = []"""
+        dead = []
+        started = False
     if data[:5] == 'DEATH':
         stuff = data[5:].split('=')
         p1 = stuff[0].split('+')
@@ -69,6 +73,9 @@ while True:
                     new_info.append(i)
             info = new_info[:]
             dead.append(int(p2[1]))
+    elif data == 'STARTED':
+        started = True
+        left = len(info)
     else:
         stuff = eval(data)
         new_info = [stuff]
@@ -78,6 +85,6 @@ while True:
                     new_info.append(i)
             info = new_info[:]
         for p in players:
-            to_send = ('DATA'+str(info)).encode('utf-8')
+            to_send = ('DATA' + str(info)).encode('utf-8')
             to_send = zlib.compress(to_send)
             sock.sendto(to_send, players[p])
