687f2aeb04c346e3b8beb6a9232c6135774c4749
WildHonestFur <137202612+WildHonestFur@users.noreply.github.com>
2024-11-26 06:50:18 -0800

    Update main.py
    Fixed Errors, Added Features

--- main.py
+++ main.py
@@ -14,25 +14,29 @@ font2 = pygame.font.Font('freesansbold.ttf', 30)
 font3 = pygame.font.Font('freesansbold.ttf', 45)
 img = pygame.image.load('trophy.png')
 img = pygame.transform.scale(img, (200, 200))
+one, two, three = [pygame.transform.scale(pygame.image.load(f'{n}.png'), (200, 200)) for n in ['one', 'two', 'three']]
 
-
-# To change
+# Change for player
 code = 1
-tx = 600
-ty = -600
+SELF_IP = '192.168.1.4'
+origx = 600
+origy = 600
 color = (250, 0, 0)
 color2 = (200, 50, 50)
 
 speed = 1.5
 size = 1225
 darts = []
-since = 1.2
+since = 2
+tx = origx
+ty = origy
 rl_ud = [False, False]
 run = True
 invis = False
 power = 'invisibility'
 select = 1
-powers = {'speed': (800, 500), 'invisibility': (900, 400), 'turret': (800, 500), 'teleport': (500, 1)}
+powers = {'homing': (800, 550), 'speed': (800, 500), 'invisibility': (900, 450), 'rapid': (650, 350),
+          'teleport': (500, 1)}
 ptime = 0
 name = ''
 use_power = [False, 0]
@@ -42,6 +46,7 @@ win = ['Well played, champion! Victory achieved!', 'Incredible! You’re the ult
 choice = random.choice(win)
 end = ''
 place = ''
+started = 0
 
 gamedata = []
 
@@ -49,7 +54,6 @@ HOST_IP = "192.168.15.167"
 HOST_PORT = 1234
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-SELF_IP = '192.168.15.168'
 SELF_PORT = 1234
 sock.bind((SELF_IP, SELF_PORT))
 
@@ -64,7 +68,7 @@ def fix(s):
 
 
 def listen():
-    global state, end, place, gamedata
+    global state, end, place, gamedata, started, darts
     while run:
         info, address = sock.recvfrom(2048)
         info = zlib.decompress(info)
@@ -75,10 +79,14 @@ def listen():
         elif info[:3] == 'WIN':
             state = 'win'
             place = 'first'
+            time.sleep(1)
         elif info[:4] == 'DATA':
             gamedata = fix(info[4:])
         elif info == 'START' and state == 'wait':
             state = 'game'
+            started = 0
+            gamedata = []
+            darts = []
 
 
 def draw_rectangle(s, x, y, width, height, c, t_x, t_y, rotation):
@@ -117,32 +125,36 @@ while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
+        if started >= 150:
+            if (pygame.key.get_pressed()[pygame.K_d] or pygame.key.get_pressed()[pygame.K_RIGHT]) and tx < size / 2:
+                tx += speed
+                rl_ud[0] = True
+            elif (pygame.key.get_pressed()[pygame.K_a] or pygame.key.get_pressed()[pygame.K_LEFT]) and tx > -size / 2:
+                tx -= speed
+                rl_ud[0] = True
+            if (pygame.key.get_pressed()[pygame.K_w] or pygame.key.get_pressed()[pygame.K_UP]) and ty > -size / 2:
+                ty -= speed
+                rl_ud[1] = True
+            elif (pygame.key.get_pressed()[pygame.K_s] or pygame.key.get_pressed()[pygame.K_DOWN]) and ty < size / 2:
+                ty += speed
+                rl_ud[1] = True
 
-        if pygame.key.get_pressed()[pygame.K_d] and tx < size / 2:
-            tx += speed
-            rl_ud[0] = True
-        elif pygame.key.get_pressed()[pygame.K_a] and tx > -size / 2:
-            tx -= speed
-            rl_ud[0] = True
-        if pygame.key.get_pressed()[pygame.K_w] and ty > -size / 2:
-            ty -= speed
-            rl_ud[1] = True
-        elif pygame.key.get_pressed()[pygame.K_s] and ty < size / 2:
-            ty += speed
-            rl_ud[1] = True
+            if sum(rl_ud) == 2:
+                speed = 1
+            else:
+                speed = 1.5
 
-        if sum(rl_ud) == 2:
-            speed = 1
-        else:
-            speed = 1.5
-
-        rl_ud = [False, False]
-
-        if pygame.mouse.get_pressed(3)[0] and since > 1.2:
-            darts.append(
-                [[tx - 20 * cos, ty + 20 * sin], (255,), -math.asin(sin) + (2 * math.asin(sin) + math.pi) * (cos > 0),
-                 0])
-            since = 0
+            rl_ud = [False, False]
+            gap = 2.5
+            if use_power[0] and power == 'rapid':
+                gap = 1.2
+            if pygame.mouse.get_pressed(3)[0] and since > gap:
+                darts.append(
+                    [[tx - 20 * cos, ty + 20 * sin], (255,),
+                     -math.asin(sin) + (2 * math.asin(sin) + math.pi) * (cos > 0),
+                     0])
+                since = 0
 
         screen.fill((121, 135, 130), pygame.Rect(0, 0, w, h))
         pygame.draw.polygon(screen, (188, 204, 198),
@@ -158,18 +170,25 @@ while run:
             pygame.draw.circle(screen, color + d[1], (450 - tx + d[0][0], 350 - ty + d[0][1]), 5)
         for d in range(len(darts)):
             darts[d][3] += 1
-            if darts[d][3] > 90 and darts[d][1][0] >= 20:
-                darts[d][1] = (darts[d][1][0] - 20,)
-            darts[d][0][0] += math.cos(darts[d][2]) * 3.6
-            darts[d][0][1] -= math.sin(darts[d][2]) * 3.6
+            if darts[d][3] > 80 + 20*(use_power[0] and power == 'homing') and darts[d][1][0] >= 30:
+                darts[d][1] = (darts[d][1][0] - 30,)
+            if use_power[0] and power == 'homing':
+                hp = math.dist(((mx + tx - 450), (my + ty - 350)), (darts[d][0][0], darts[d][0][1]))
+                darts[d][0][0] += 3.6 * ((mx + tx - 450) - darts[d][0][0]) / hp
+                darts[d][0][1] -= -3.6 * ((my + ty - 350) - darts[d][0][1]) / hp
+            else:
+                darts[d][0][0] += math.cos(darts[d][2]) * 3.6
+                darts[d][0][1] -= math.sin(darts[d][2]) * 3.6
         newdarts = []
         for d in darts:
-            if d[1][0] >= 20:
+            if d[1][0] >= 30 and (not (use_power[0] and power == 'homing') or math.dist(((mx + tx - 450), (my + ty - 350)), (d[0][0], d[0][1])) > 3):
                 newdarts.append(d)
         darts = newdarts[:]
 
-        if pygame.key.get_pressed()[pygame.K_f] and ptime >= powers[power][0] and not use_power[0]:
-            use_power[0] = True
+        if started >= 150:
+            if (pygame.key.get_pressed()[pygame.K_f] or pygame.key.get_pressed()[pygame.K_SPACE]) and ptime >= \
+                    powers[power][0] and not use_power[0]:
+                use_power[0] = True
 
         if use_power[0]:
             if power == 'speed':
@@ -219,7 +238,7 @@ while run:
             textRect.center = (450, 635)
             screen.blit(text, textRect)
         else:
-            text = font.render('Press F To Use', True, (0, 0, 0))
+            text = font.render('Press F or Space To Use', True, (0, 0, 0))
             textRect = text.get_rect()
             textRect.center = (450, 635)
             screen.blit(text, textRect)
@@ -227,12 +246,23 @@ while run:
         surface.fill((0, 0, 0, 180))
         for p in gamedata:
             if not p[3]:
-                pygame.draw.circle(surface, p[4], (65+p[1][0]/size*130, 65+p[1][1]/size*130), 3)
+                pygame.draw.circle(surface, p[4], (65 + p[1][0] / size * 130, 65 + p[1][1] / size * 130), 3)
         pygame.draw.circle(surface, color, (65 + tx / size * 130, 65 + ty / size * 130), 3)
         screen.blit(surface, (750, 550))
+        if started == 150:
+            sock.sendto(bytes('STARTED', 'utf-8'), (HOST_IP, HOST_PORT))
+        if started >= 150:
+            since += 0.1
+            ptime += 1 * (ptime < powers[power][0])
+        else:
+            if started < 50:
+                screen.blit(three, (350, 250))
+            elif started < 100:
+                screen.blit(two, (350, 250))
+            else:
+                screen.blit(one, (350, 250))
+            started += 1
         game.blit(screen, (0, 0))
-        since += 0.1
-        ptime += 1 * (ptime < powers[power][0])
         if use_power[0]:
             use_power[1] += 1
         data = ['\'' + name + '\'', str((tx, ty)), str(-math.asin(sin) + (2 * math.asin(sin) + math.pi) * (cos > 0)),
@@ -248,12 +278,13 @@ while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
         screen.fill((188, 204, 198), pygame.Rect(0, 0, w, h))
         for val in range(size // 25 + 1):
-            pygame.draw.line(screen, (204, 216, 211), (450 - tx - size / 2, 350 - ty + 25 * val - size / 2),
-                             (450 - tx + size / 2, 350 - ty + 25 * val - size / 2), 2)
-            pygame.draw.line(screen, (204, 216, 211), (450 - tx + 25 * val - size / 2, 350 - ty - size / 2),
-                             (450 - tx + 25 * val - size / 2, 350 - ty + size / 2), 2)
+            pygame.draw.line(screen, (204, 216, 211), (450 - size / 2, 350 + 25 * val - size / 2),
+                             (450 + size / 2, 350 + 25 * val - size / 2), 2)
+            pygame.draw.line(screen, (204, 216, 211), (450 + 25 * val - size / 2, 350 - size / 2),
+                             (450 + 25 * val - size / 2, 350 + size / 2), 2)
         pygame.draw.polygon(screen, (0, 0, 0), [(50, 50), (50, 650), (850, 650), (850, 50)])
         pygame.draw.polygon(screen, (255, 255, 255), [(52, 52), (52, 648), (848, 648), (848, 52)])
         if state == 'end':
@@ -280,16 +311,16 @@ while run:
         if pygame.mouse.get_pressed(3)[0] and 320 < mx < 580 and 520 < my < 600:
             state = 'start'
             choice = random.choice(win)
-            tx = 0
-            ty = 0
+            tx = origx
+            ty = origy
             speed = 1.5
-            since = 1.2
+            since = 2
             rl_ud = [False, False]
             invis = False
             ptime = 0
             use_power = [False, 0]
             name = ''
-            time.sleep(0.2)
+            time.sleep(0.3)
 
     elif state == 'start':
         w, h = pygame.display.get_surface().get_size()
@@ -299,17 +330,24 @@ while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_BACKSPACE:
-                    name = name[:-1]
-                elif len(name) < 40:
-                    name += event.unicode
+                if len(name) < 40:
+                    try:
+                        if ord(event.unicode) == 32 or 48 <= ord(event.unicode) <= 57 or 65 <= ord(
+                                event.unicode) <= 90 or 97 <= ord(event.unicode) <= 122:
+                            name += event.unicode
+                    except TypeError:
+                        pass
+        if pygame.key.get_pressed()[pygame.K_BACKSPACE]:
+            name = name[:-1]
+            time.sleep(0.1)
         screen.fill((188, 204, 198), pygame.Rect(0, 0, w, h))
         for val in range(size // 25 + 1):
-            pygame.draw.line(screen, (204, 216, 211), (450 - tx - size / 2, 350 - ty + 25 * val - size / 2),
-                             (450 - tx + size / 2, 350 - ty + 25 * val - size / 2), 2)
-            pygame.draw.line(screen, (204, 216, 211), (450 - tx + 25 * val - size / 2, 350 - ty - size / 2),
-                             (450 - tx + 25 * val - size / 2, 350 - ty + size / 2), 2)
+            pygame.draw.line(screen, (204, 216, 211), (450 - size / 2, 350 + 25 * val - size / 2),
+                             (450 + size / 2, 350 + 25 * val - size / 2), 2)
+            pygame.draw.line(screen, (204, 216, 211), (450 + 25 * val - size / 2, 350 - size / 2),
+                             (450 + 25 * val - size / 2, 350 + size / 2), 2)
         pygame.draw.polygon(screen, (0, 0, 0), [(50, 50), (50, 650), (850, 650), (850, 50)])
         pygame.draw.polygon(screen, (255, 255, 255), [(52, 52), (52, 648), (848, 648), (848, 52)])
         text = font3.render('Battle Royale', True, color)
@@ -336,45 +374,67 @@ while run:
         screen.blit(text, textRect)
         text = font2.render('Power:', True, (0, 0, 0))
         textRect = text.get_rect()
-        textRect.center = (180, 400)
+        textRect.center = (170, 395)
         screen.blit(text, textRect)
         if select != 1:
-            pygame.draw.rect(screen, (171, 171, 171), pygame.Rect(250, 385, 150, 30))
+            pygame.draw.rect(screen, (171, 171, 171), pygame.Rect(250, 355, 150, 30))
         else:
-            pygame.draw.rect(screen, color, pygame.Rect(250, 385, 150, 30))
+            pygame.draw.rect(screen, color, pygame.Rect(250, 355, 150, 30))
             power = 'invisibility'
         if select != 2:
-            pygame.draw.rect(screen, (171, 171, 171), pygame.Rect(430, 385, 150, 30))
+            pygame.draw.rect(screen, (171, 171, 171), pygame.Rect(430, 355, 150, 30))
         else:
-            pygame.draw.rect(screen, color, pygame.Rect(430, 385, 150, 30))
+            pygame.draw.rect(screen, color, pygame.Rect(430, 355, 150, 30))
             power = 'teleport'
         if select != 3:
-            pygame.draw.rect(screen, (171, 171, 171), pygame.Rect(610, 385, 150, 30))
+            pygame.draw.rect(screen, (171, 171, 171), pygame.Rect(610, 355, 150, 30))
         else:
-            pygame.draw.rect(screen, color, pygame.Rect(610, 385, 150, 30))
+            pygame.draw.rect(screen, color, pygame.Rect(610, 355, 150, 30))
             power = 'speed'
+        if select != 4:
+            pygame.draw.rect(screen, (171, 171, 171), pygame.Rect(250, 405, 150, 30))
+        else:
+            pygame.draw.rect(screen, color, pygame.Rect(250, 405, 150, 30))
+            power = 'homing'
+        if select != 5:
+            pygame.draw.rect(screen, (171, 171, 171), pygame.Rect(430, 405, 150, 30))
+        else:
+            pygame.draw.rect(screen, color, pygame.Rect(430, 405, 150, 30))
+            power = 'rapid'
         text = font.render('Invisibility', True, (0, 0, 0))
         textRect = text.get_rect()
-        textRect.center = (325, 400)
+        textRect.center = (325, 370)
         screen.blit(text, textRect)
         text = font.render('Teleport', True, (0, 0, 0))
         textRect = text.get_rect()
-        textRect.center = (505, 400)
+        textRect.center = (505, 370)
         screen.blit(text, textRect)
         text = font.render('Speed', True, (0, 0, 0))
         textRect = text.get_rect()
-        textRect.center = (685, 400)
+        textRect.center = (685, 370)
+        screen.blit(text, textRect)
+        text = font.render('Homing Shots', True, (0, 0, 0))
+        textRect = text.get_rect()
+        textRect.center = (325, 420)
+        screen.blit(text, textRect)
+        text = font.render('Rapid Fire', True, (0, 0, 0))
+        textRect = text.get_rect()
+        textRect.center = (505, 420)
         screen.blit(text, textRect)
         if pygame.mouse.get_pressed(3)[0]:
             if 320 < mx < 580 and 520 < my < 600:
                 state = 'wait'
-                time.sleep(0.2)
-            if 250 < mx < 400 and 385 < my < 415:
+                time.sleep(0.3)
+            if 250 < mx < 400 and 355 < my < 385:
                 select = 1
-            if 430 < mx < 580 and 385 < my < 415:
+            if 430 < mx < 580 and 355 < my < 385:
                 select = 2
-            if 610 < mx < 760 and 385 < my < 415:
+            if 610 < mx < 760 and 355 < my < 385:
                 select = 3
+            if 250 < mx < 400 and 405 < my < 435:
+                select = 4
+            if 430 < mx < 580 and 405 < my < 435:
+                select = 5
         game.blit(screen, (0, 0))
     elif state == 'wait':
         w, h = pygame.display.get_surface().get_size()
@@ -384,12 +444,13 @@ while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
         screen.fill((188, 204, 198), pygame.Rect(0, 0, w, h))
         for val in range(size // 25 + 1):
-            pygame.draw.line(screen, (204, 216, 211), (450 - tx - size / 2, 350 - ty + 25 * val - size / 2),
-                             (450 - tx + size / 2, 350 - ty + 25 * val - size / 2), 2)
-            pygame.draw.line(screen, (204, 216, 211), (450 - tx + 25 * val - size / 2, 350 - ty - size / 2),
-                             (450 - tx + 25 * val - size / 2, 350 - ty + size / 2), 2)
+            pygame.draw.line(screen, (204, 216, 211), (450 - size / 2, 350 + 25 * val - size / 2),
+                             (450 + size / 2, 350 + 25 * val - size / 2), 2)
+            pygame.draw.line(screen, (204, 216, 211), (450 + 25 * val - size / 2, 350 - size / 2),
+                             (450 + 25 * val - size / 2, 350 + size / 2), 2)
         pygame.draw.polygon(screen, (0, 0, 0), [(50, 50), (50, 650), (850, 650), (850, 50)])
         pygame.draw.polygon(screen, (255, 255, 255), [(52, 52), (52, 648), (848, 648), (848, 52)])
         text = font3.render('Waiting for players...', True, (0, 0, 0))
